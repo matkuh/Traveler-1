f3f5e222bbc0eae2fc0adb75c374dad604d01949
blueDogs <danielchicchon@gmail.com>
2019-08-11 09:17:48 -0700

    add express web framework and initial create react app in client folder

---
+++ server/routes/index.js
@@ -0,0 +1,12 @@
+const path = require("path")
+const router = require("express").Router()
+
+router.route("/", () => {
+    console.log("HELLO!")
+})
+
+router.use(function (req, res) {
+    res.sendFile(path.join(__dirname, "../client/public/index.html"))
+});
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const passport = require("passport")
+const routes = require("./routes")
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// app.use(routes)
+app.get("/", function (req, res) {
+    res.send("Hello")
+})
+
+app.listen(PORT, () => {
+    console.log("App is running on PORT " + PORT)
+})
