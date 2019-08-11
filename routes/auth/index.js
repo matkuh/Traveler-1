77febc61b873f8fc455f32ca00532bf3cc307eaf
blueDogs <danielchicchon@gmail.com>
2019-08-11 12:41:32 -0700

    move server files to root directory to push to heroku

--- routes/auth/index.js
+++ routes/auth/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const passport = require('passport');
-const db = require('../../models');
+const db = require('../../../models');
 
 // shorthand
 const log = console.log
